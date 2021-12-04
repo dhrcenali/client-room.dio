1119f17cd43ba46fddac7c1575a0264b328b5fba
DanielCenali <danielcenali@gmail.com>
2021-12-04 18:44:29 -0300

    primeiro commit

---
+++ src/app/create-room/create-room.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Room } from '../room';
+import { RoomService } from '../room.service';
+
+@Component({
+  selector: 'app-create-room',
+  templateUrl: './create-room.component.html',
+  styleUrls: ['./create-room.component.css']
+})
+export class CreateRoomComponent implements OnInit {
+  room: Room = new Room ();
+  submitted = false
+
+  constructor(
+    private roomService: RoomService,
+    private router: Router) {}
+
+  ngOnInit() {
+  }
+
+    newRoom(): void{
+      this.submitted=false;
+      this.room= new Room();
+    }
+
+    save(){
+      this.roomService.createRoom(this.room)
+        .subscribe(data=>console.log(data), error => console.log(error));
+        this.room= new Room();
+        this.gotolist();  
+    }
+
+    onSubmit(){
+      this.submitted = true;
+      this.save();
+    }
+    gotolist(){
+      this.router.navigate(['/rooms '])
+    }
+}
